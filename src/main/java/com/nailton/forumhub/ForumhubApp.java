1f57c9c90116c7420524c50487299fac1113239a
nailton-vieira <nailtonvieira@gmail.com>
2024-07-04 15:02:17 -0300

    Adicionando arquivo base ao repo

---
+++ src/main/java/com/nailton/forumhub/ForumhubApp.java
@@ -0,0 +1,13 @@
+package com.nailton.forumhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumhubApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumhubApp.class, args);
+	}
+
+}
