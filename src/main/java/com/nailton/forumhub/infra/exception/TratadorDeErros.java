f995d43515e78094661d4fd67f70005beb92cc73
nailton-vieira <nailtonvieira@gmail.com>
2024-07-09 13:58:37 -0300

    Enviando security token jwt e dependencias ao repo

---
+++ src/main/java/com/nailton/forumhub/infra/exception/TratadorDeErros.java
@@ -0,0 +1,14 @@
+package com.nailton.forumhub.infra.exception;
+import com.nailton.forumhub.domain.ValidacaoException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+
+public class TratadorDeErros {
+    @ExceptionHandler(ValidacaoException.class)
+    public ResponseEntity<String> tratarErroRegraDeNegocio(ValidacaoException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+}
